817b8899865fb9ccca7f2c2ce2c9cc98766f8ba6
ivadelina <kapranova.milla@yandex.ru>
2021-10-05 11:26:44 +0300

    Задача добавлена

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,39 @@
 function Student(name, gender, age) {
-    // Ваш код
-
-}
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+};
+let student1 = new Student('Людмила', 'женский', 29);
+let student2 = new Student('Егор', 'мужской', 31);
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
-}
+  this.subject = subjectName;
+};
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if(this.marks === undefined){ 
+    this.marks = [mark];
+  } else {
+    this.marks.push(mark)
+    };
+};
+Student.prototype.addMarks = function (...mark) {
+  if(this.marks === undefined){ 
+    this.marks = mark;
+  } else {
+    this.marks.push(...mark)
+    };
+};
+Student.prototype.getAverage = function () {
+  let sum = 0;
+  for(let i in this.marks) {
+    sum += this.marks[i];
+  };
+  let avg = sum / this.marks.length;
+  return avg;
+  };
+  Student.prototype.exclude = function (reason) {
+    delete this.marks;
+    delete this.subject;
+    this.excluded = reason;
+  };
